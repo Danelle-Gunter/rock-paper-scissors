9ac43715b36d7c236f3afae87658a633fd6f28a1
Danelle <rhowia@gmail.com>
2022-08-06 15:49:34 -0400

    Added a player's selection in the script

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,32 @@
  * 
  * @returns {string} A random selection from the array
  */
-function getComputerChoice() {
+let computerSelection = function() {
     let choices = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * choices.length);
     return choices[index];
-}
+};
 
-console.log(getComputerChoice());
+/**
+ * Asks player for input and returns that input
+ * 
+ * @returns {string} Player's input
+ */
+let playerSelection = function() {
+    let choice = prompt("Select ROCK, PAPER, or SCISSORS: ").toLowerCase();    
+    return choice;
+};
+
+/**
+ * Plays a single round of the game
+ * 
+ * @param {string} computerSelection A choice from that function
+ * @param {string} playerSelection An input from that function
+ * 
+ */
+// function playGame(computerSelection, playerSelection) {
+
+// }
+
+console.log(playerSelection());
+console.log(computerSelection());
